0f878c0f95bff48b20d9f19d821b7d6b50137e85
Sk Sofiquee Fiaz <sofiqueefiaz2022@vitbhopal.ac.in>
2023-09-02 23:15:50 +0530

    Adding swapping two numbers

--- swap.java
+++ swap.java
@@ -1,5 +1,13 @@
 public class swap {
     public static void main(String[] args) {
-        
+        int a = 10;
+        int b =5;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a);
+        System.out.println(b);
     }
 }
