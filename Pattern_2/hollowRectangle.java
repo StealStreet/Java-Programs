c998cf60fce896aa30277fb09ed6a2dfae9fa46e
Sk Sofiquee Fiaz <sofiqueefiaz2022@vitbhopal.ac.in>
2023-09-03 00:10:47 +0530

    Adding Pattern file

---
+++ Pattern_2/hollowRectangle.java
@@ -0,0 +1,29 @@
+package Pattern_2;
+
+import java.util.Scanner;
+
+public class hollowRectangle {
+
+    public static void hollow_rectangle(int row, int col) {
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (i == 1 || i == row || j == 1 || j == col) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter the number of rows : ");
+        Scanner scan = new Scanner(System.in);
+        int row = scan.nextInt();
+        System.out.print("Enter the number of columns : ");
+        int col = scan.nextInt();
+        hollow_rectangle(row, col);
+        scan.close();
+    }
+}
